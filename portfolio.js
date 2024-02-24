641e2c43b5da8585873c46af60fecd8c6b94c5e9
patilsanketofficial <sp7974682@gmail.com>
2024-02-24 13:08:25 +0530

    initial commit

---
+++ portfolio.js
@@ -0,0 +1,40 @@
+// <! -- ==============================CREATE AUTOMATIC NAVBAR SCROLL================================= -->
+
+const section=document.querySelectorAll('section')
+const navLink=document.querySelectorAll('header nav a')
+window.onscroll=()=>
+{
+    section.forEach(sec=> 
+    {
+        let top=window.scrollY;
+        let offset=sec.offsetTop - 150;
+        let height=sec.offsetHeight;
+        let id=sec.getAttribute('id')
+        if(top>=offset && top<offset + height)
+        {
+            navLink.forEach(links=>
+                {
+                    links.classList.remove('active')
+                    document.querySelector('header nav a[href*='+id+']').classList.add('active')
+                })
+        }   
+    })
+
+// <! -- ==============================CREATE STICKY NAV BAR ================================= -->
+
+var header=document.querySelector('header')
+header.classList.toggle('sticky',window.scrollY>100)
+menubar.classList.remove('bx-x')
+navbar.classList.remove('active')
+};
+
+
+// <! -- ==============================CREATE A TOGGLE CHANGER ================================= -->
+
+let menubar=document.querySelector('#menu')
+let navbar=document.querySelector('.navbar')
+menubar.onclick= ()=>
+{
+    menubar.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+} 
